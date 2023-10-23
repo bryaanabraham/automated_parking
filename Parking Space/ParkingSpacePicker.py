08596b5c5170e6869d758bc9834876e0b7c2fd8f
Bryan Abraham <bryan.ab.lp@gmail.com>
2023-10-23 23:41:13 +0530

    Empty Parking Space detection

---
+++ Parking Space/ParkingSpacePicker.py
@@ -0,0 +1,30 @@
+import cv2
+import pickle
+
+width, height = 107, 48
+#Pickle overwrites existing files so we check if one already exists
+try:
+    with open('CarParkPos', 'rb') as f:
+        posList=pickle.load(f)
+except:
+    posList = []
+
+
+def mouseclick(events, x, y, flags, params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1,y1=pos
+            if x1<x<x1+width and y1<y<y1+height:
+                posList.pop(i)
+    with open('CarParkPos','wb')as f:
+        pickle.dump(posList, f)
+
+while True:
+    img = cv2.imread('assets/carParkImg.png')
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+    cv2.imshow('image', img)
+    cv2.setMouseCallback("image", mouseclick)
+    cv2.waitKey(1)
